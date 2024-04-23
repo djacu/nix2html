2878b384e097da8789ca18f725ca3831e7130cc8
Daniel Baker <daniel.n.baker@gmail.com>
2024-04-22 21:03:04 -0700

    Remove old _module.args code.

--- eval.nix
+++ eval.nix
@@ -5,20 +5,8 @@ let
       modules = [
         ./elements/a.nix
         ./config.nix
-        #(
-        #  { ... }:
-        #  {
-        #    config._module.args = {
-        #      paths = {
-        #        lib = ./lib;
-        #        elements = ./elements;
-        #        attributes = ./attributes;
-        #        modules = ./modules;
-        #      };
-        #    };
-        #  }
-        #)
       ];
+
       specialArgs = {
         # moved to specialArgs because _module.args was causing infinite recursion errors
         paths = {
